1ea26ae7531afc9ed6494920344aab39b760286f
Jesus Herrera <jesusherrera1594@gmail.com>
2025-08-19 18:48:24 -0600

    set stereo as mandatory in plugin for the moment

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -103,31 +103,10 @@ void DelayPJAudioProcessor::releaseResources()
     // spare memory, etc.
 }
 
-#ifndef JucePlugin_PreferredChannelConfigurations
-bool DelayPJAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+bool DelayPJAudioProcessor::isBusesLayoutSupported(const BusesLayout& layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
-
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
-
-    return true;
-  #endif
+    return layouts.getMainInputChannelSet() == juce::AudioChannelSet::stereo();
 }
-#endif
 
 void DelayPJAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, [[maybe_unused]] juce::MidiBuffer& midiMessages)
 {
