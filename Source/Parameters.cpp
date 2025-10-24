9f1ba02f3d7514fffeacf03730101d05f879b8c1
Jesus Herrera <jesusherrera1594@gmail.com>
2025-10-23 19:25:43 -0600

    format the display value in the UI

--- Source/Parameters.cpp
+++ Source/Parameters.cpp
@@ -16,6 +16,22 @@ static void castParameter(juce::AudioProcessorValueTreeState& apvts, const juce:
     jassert(destination);
 }
 
+static juce::String stringFromMilliseconds(float value, int) {
+    if (value < 10.0f) {
+        return juce::String(value, 2) + " ms";
+    } else if (value < 100.0f){
+        return juce::String(value, 1) + " ms";
+    } else if (value < 1000.0f) {
+        return juce::String(int(value))  + " ms";
+    } else {
+        return juce::String(value * 0.001f, 2) + " s";
+    }
+}
+
+static juce::String stringFromDecibels(float value, int) {
+    return juce::String(value, 1) + " dB";
+}
+
 Parameters::Parameters(juce::AudioProcessorValueTreeState& apvts){
     castParameter(apvts, gainParamID, gainParam);
     castParameter(apvts, delayTimeParamID, delayTimeParam);
@@ -28,7 +44,9 @@ juce::AudioProcessorValueTreeState::ParameterLayout Parameters::createParameterL
                                                            gainParamID,
                                                            "Output Gain",
                                                            juce::NormalisableRange<float>{ -12.0f, 12.0f},
-                                                           0.0f
+                                                           0.0f,
+                                                           juce::AudioParameterFloatAttributes()
+                                                           .withStringFromValueFunction(stringFromDecibels)
                                                            )
                );
     layout.add(
@@ -36,7 +54,9 @@ juce::AudioProcessorValueTreeState::ParameterLayout Parameters::createParameterL
                                                            delayTimeParamID,
                                                            "Delay Time",
                                                            juce::NormalisableRange<float> {minDelayTime, maxDelayTime, 0.001f, 0.25f },
-                                                           100.0f
+                                                           100.0f,
+                                                           juce::AudioParameterFloatAttributes()
+                                                           .withStringFromValueFunction(stringFromMilliseconds)
                                                            )
                );
     return layout;
