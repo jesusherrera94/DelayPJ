1f6f743fce505680cc82ef0b8b019247bcc3f6e7
Jesus Herrera <jesusherrera1594@gmail.com>
2025-11-04 19:48:31 -0600

    draw knob background

--- Source/LookAndFeel.cpp
+++ Source/LookAndFeel.cpp
@@ -15,6 +15,9 @@ RotaryKnobLookAndFeel::RotaryKnobLookAndFeel() {
     setColour(juce::Slider::textBoxTextColourId, Colors::knob::label);
 }
 
-void RotaryKnobLookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height, float sliderPos, float rotaryStartAngle, float rotaryEndAngle, juce::Slider &slider) {
-    
+void RotaryKnobLookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int width, [[maybe_unused]] int height, float sliderPos, float rotaryStartAngle, float rotaryEndAngle, juce::Slider &slider) {
+    auto bounds = juce::Rectangle<int>(x,y,width,width).toFloat();
+    auto knobRect = bounds.reduced(10.0f, 10.0f);
+    g.setColour(Colors::knob::outline);
+    g.fillEllipse(knobRect);
 }
